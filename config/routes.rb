c5e6a00f927b2f1ef74d938f9ab370f2292cb476
Mickael Toledano gh api mikatol06@gmail.com <mikatol06@gmail.com>
2022-03-24 13:00:40 +0100

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'movies/new'
+  get 'movies/create'
+  get 'movies/update'
+  get 'movies/edit'
+  get 'movies/destroy'
+  get 'movies/show'
+  get 'movies/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
